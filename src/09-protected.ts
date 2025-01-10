acbd3692b1ecd0a305ad8bf27a9e6b88e01950af
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-01-10 10:31:09 +0100

    Acceso protegido

---
+++ src/09-protected.ts
@@ -0,0 +1,39 @@
+export class Animal {
+    constructor(protected name: string){}
+
+    move(){
+        console.log('Moving along!');
+    }
+
+    greeting() {
+        return `Hello, I'm ${this.name}`;
+    }
+
+    protected doSomething() {
+        console.log('do something')
+    }
+}
+
+export class Dog extends Animal{
+
+    constructor(name: string, public owner: string){
+        super(name);
+    }
+
+    woof(times: number): void {
+        for (let index = 0; index < times; index++) {
+            console.log(`woof! ${this.name}`);
+        }
+        this.doSomething();
+    }
+
+    move() {
+        console.log('moving as a dog');
+        super.move();
+    }
+}
+
+
+const cheis = new Dog('cheis', 'nico');
+cheis.woof(1);
+cheis.move();
