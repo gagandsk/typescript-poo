4a8a241080a4d6b8844d5dc1a5bac5b8ac576017
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-01-12 22:53:46 +0100

    Generics en metodos

--- src/app/services/base-http.service.ts
+++ src/app/services/base-http.service.ts
@@ -1,3 +1,4 @@
+import { UpdateProductDto } from "../dtos/product.dto";
 import { Category } from "../models/category.model";
 import { Product } from "../models/product.model";
 
@@ -7,11 +8,16 @@ export class BaseHttpService<TypeClass> {
     //data: TypeClass[] = [];
 
     constructor(
-        private url: string
+        protected url: string
     ){}
 
     async getAll() {
         const { data }  = await axios.get<TypeClass[]>(this.url);
+        return data;
+    }
+
+    async updateProduct<ID, DTO>(id: ID, changes: DTO) {
+        const { data } = await axios.put(`${this.url}/${id}`, changes);
         return data;
     }
 }
@@ -21,10 +27,12 @@ export class BaseHttpService<TypeClass> {
     const productsService = new BaseHttpService<Product>(url1);
     const rta = await productsService.getAll();
     console.log('products:',rta.length);
+    productsService.updateProduct<Product['id'], UpdateProductDto>(1, {
+        title: 'generic in methods , product example'
+    });
 
     const url2 = 'https://api.escuelajs.co/api/v1/categories';
     const categoryService = new BaseHttpService<Category>(url2);
     const rta2 = await categoryService.getAll();
     console.log('categories:',rta2.length);
-
 })();
